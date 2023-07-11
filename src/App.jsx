c70a0ddaee1fb23791a85e322912cbe48650954b
Saani765 <saani765@gmail.com>
2023-07-11 20:11:16 +0530

    modified after some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,135 @@
-import React from "react";
-import { Typography,AppBar,Card,CardActions,CardContent,CardMedia,CssBaseline,Grid,Toolbar,Container} from "@mui/material";
-import PhotoCameraIcon from '@mui/icons-material/PhotoCamera';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import { makeStyles } from '@mui/core/styles';
+import CameraIcon from '@mui/icons-material/PhotoCamera';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const useStyles=makeStyles((theme)=>({
-    container:{
-        backgroundColor : theme.pallete.background.paper,
-        padding : theme.spacing(8,0,6)
-    }
-}));
-
-
-const App=()=>
-{   const classes=useStyles();
-    return (
-        <>
-          <CssBaseline/>
-          <AppBar position="relative">
-                <Toolbar>
-                    <PhotoCameraIcon/>
-                    <Typography variant="h6"> 
-                       Photo Album
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-          <main>
-            <div className={classes.container}>
-                <Container maxWidth="sm" >
-                    <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
-                        Photo Album
-                    </Typography>
-                    <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                        Hello everyone this is a photo album and I am enjoying making this project website for my learning process
-                    </Typography>
-                    <div>
-                        <Grid container spacing={3} justify="center">
-                            <Grid item>
-                                <Button variant="contained" color="primary">
-                                    See my photos
-
-                                </Button>
-                            </Grid>
-                            <Grid item>
-                                <Button variant="outlined" color="primary">
-                                    Know more about them
-
-                                </Button>
-                            </Grid>
-                        </Grid>
-                    </div>
-                </Container>
-            </div>
-            
-          </main>
-        
-        
-        </>
-    );
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+      https://github.com/Saani765
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default App;
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function Album() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <CameraIcon sx={{ mr: 2 }} />
+          <Typography variant="h6" color="inherit" noWrap>
+            Photo Album 
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <Box
+          sx={{
+            bgcolor: 'warning.main',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+            <Typography variant="h5" align="center" color="text.secondary" paragraph>
+             This is a sample photo album I created for learning material ui and its properties
+            </Typography>
+            <Stack
+              sx={{ pt: 4 }}
+              direction="row"
+              spacing={2}
+              justifyContent="center"
+            >
+              <Button variant="contained">Main call to action</Button>
+              <Button variant="contained">Secondary action</Button>
+            </Stack>
+          </Container>
+        </Box>
+        <Container color ="black" sx={{ py: 8 }} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                >
+                  <CardMedia
+                    component="div"
+                    sx={{
+                      // 16:9
+                      pt: '56.25%',
+                    }}
+                    image="https://source.unsplash.com/random?wallpapers"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the
+                      content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" variant="outlined">View</Button>
+                    <Button size="small" variant ="outlined">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+      <Box sx={{ bgcolor: 'success.main', p: 6 }} component="footer">
+        <Typography variant="h6" align="center" gutterBottom>
+          Saanidhya Singh
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="text.secondary"
+          component="p"
+        >
+          Made for learning purposes 
+        </Typography>
+        <Copyright />
+      </Box>
+      {/* End footer */}
+    </ThemeProvider>
+  );
+}
